dabfcfc4259eb79674b3f53b8683668dd3b5357e
Michele Saba <michelemsaba@gmail.com>
2019-06-25 13:48:46 -0500

    file structure setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from './components/header';
+import Footer from './components/footer';
+import Counter from './components/counter';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Counter />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
